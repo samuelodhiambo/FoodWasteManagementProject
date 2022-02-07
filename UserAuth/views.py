c5fd365f0483ca1f860fecf8699f51faa7cfa38f
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-02-07 21:44:56 +0300

    test: User-Auth - Testing user authentication (register, login and logout)

--- UserAuth/views.py
+++ UserAuth/views.py
@@ -1,10 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 # Create your views here.
-def login(request, template='Login.html'):
+def signin(request, template='Login.html'):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request=request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('home')
+        error = 'Credentials do not match'
+        return render(request, template, {'error': error})
     return render(request, template)
 
 def register(request, template='Register.html'):
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        return render(request, template, {'form': form})
+    form = CreateUserForm()
     return render(request, template)
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
 
     
