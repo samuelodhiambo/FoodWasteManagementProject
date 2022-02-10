708c1e16891b6d0256f15b5befe6cf59e73803c6
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-02-10 15:58:00 +0300

    bug-fix: order-item fix order view bug

--- catalog/static/js/main.js
+++ catalog/static/js/main.js
@@ -1,13 +1,23 @@
 const element = document.getElementById('content');
 const modal = document.getElementById('productModal');
+let storage = window.localStorage;
 function showDropdown() {
     element.classList.toggle('show');
 }
 
-function showModal() {
+function showModal(x) {
+    const productName = document.getElementById('productName');
+    const productImage = document.getElementById("productImage");
+
+    productName.innerText = x.name;
+
+    productImage.setAttribute("src", x.image);
+
     modal.classList.toggle('show')
+
 }
 
 function closeModal() {
     modal.classList.add('show')
 }
+
