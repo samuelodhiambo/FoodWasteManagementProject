811fdb84a0e87d0bbc8fa734b979a12b0ac1fd6e
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-02-26 22:24:38 +0000

    Database config for PythonAnywhere

--- FoodWasteManager/settings.py
+++ FoodWasteManager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xol!t%*usc-23#e50kq(u@75n@&jzeifrzlpgj-%wtqj)wo&09'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -87,7 +87,7 @@ if DEBUG:
 else:
     DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'foodwastemanagem$foodwastemanager',
         'USER': 'foodwastemanagem',
         'PASSWORD': 'toortoor',
